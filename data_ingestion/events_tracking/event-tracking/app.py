51cd834e30aa43f0838ee402c4dd22f866d90d9d
Bill Metangmo <25366207+billmetangmo@users.noreply.github.com>
2024-08-29 22:11:41 +0200

    fix: change api endpoint name from hello to events

--- data_ingestion/events_tracking/event-tracking/app.py
+++ data_ingestion/events_tracking/event-tracking/app.py
@@ -46,7 +46,7 @@ def lambda_handler(event, context):
         return {
         "statusCode": 200,
         "body": json.dumps({
-            "message": "hello world",
+            "message": "Success",
             # "location": ip.text.replace("\n", "")
         }),
     }
--- data_ingestion/events_tracking/tests/integration/test_api_gateway.py
+++ data_ingestion/events_tracking/tests/integration/test_api_gateway.py
@@ -42,4 +42,4 @@ class TestApiGateway:
         response = requests.get(api_gateway_url)
 
         assert response.status_code == 200
-        assert response.json() == {"message": "hello world"}
+        assert response.json() == {"message": "Success"}
